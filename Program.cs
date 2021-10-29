2445d0ba833488acd62fdfd47ca599e2cc46ea5f
MagicMyke <mykalexander478@gmail.com>
2021-10-29 09:08:02 -0400

    version.1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Multiplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+
+            Console.WriteLine("Ingrese un numero entero:");
+            a = int.Parse(Console.ReadLine());
+
+            
+        }
+    }
+}
