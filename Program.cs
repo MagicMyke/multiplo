8b627e55789a3026eacbd09283513c6db5983346
MagicMyke <mykalexander478@gmail.com>
2021-10-29 09:19:38 -0400

    version.3

--- Program.cs
+++ Program.cs
@@ -6,18 +6,24 @@ namespace Multiplo
     {
         static void Main(string[] args)
         {
-            int a, b, multi;
+            int valor, b, multi;
 
             multi = 0;
 
             for (b = 1; b <= 5; b++)
             {
                  Console.WriteLine("Ingrese un numero entero:");
-                 a = int.Parse(Console.ReadLine());
+                 valor = int.Parse(Console.ReadLine());
+
+                 Console.WriteLine("Ingrese un numero entero:");
+                 valor = int.Parse(Console.ReadLine());
+
+                 if (valor % multi == 0)
+                 {
+
+                 } 
 
             }
-
-
 
 
         }
